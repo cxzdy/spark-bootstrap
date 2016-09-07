f329eb16024816d5cfd3ca9f934fd89441369747
zhuguangbin <zhuguangbin@360.cn>
2016-09-07 16:03:00 +0800

    upgrade streaming kafka integration to 0.10

--- spark-demo/src/main/scala/com/mvad/spark/demo/streaming/DSPRealTimeSessionization.scala
+++ spark-demo/src/main/scala/com/mvad/spark/demo/streaming/DSPRealTimeSessionization.scala
@@ -2,20 +2,17 @@ package com.mvad.spark.demo.streaming
 
 import java.util.Base64
 
-import com.mediav.data.log.LogUtils
-import kafka.serializer.StringDecoder
-import kafka.utils.{ZKStringSerializer, ZkUtils}
-import kafka.common.TopicAndPartition
+import kafka.utils._
 import org.I0Itec.zkclient.ZkClient
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming._
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.dstream.DStream
-import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaCluster, KafkaUtils, OffsetRange}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext, SparkException}
 import org.slf4j.LoggerFactory
 
@@ -50,64 +47,53 @@ object DSPRealTimeSessionization {
     val zkConnect = "zk1ss.prod.mediav.com:2191,zk2ss.prod.mediav.com:2191,zk3ss.prod.mediav.com:2191,zk12ss.prod.mediav.com:2191,zk13ss.prod.mediav.com:2191"
     val zkSessionTimeoutMs = 6000
     val zkConnectionTimeoutMs = 6000
-    val zkClient = new ZkClient(zkConnect, zkSessionTimeoutMs, zkConnectionTimeoutMs, ZKStringSerializer)
+    val zkClient = new ZkClient(zkConnect, zkSessionTimeoutMs, zkConnectionTimeoutMs)
+    val zkUtils = ZkUtils(zkClient,false)
     val zkAppPath = s"/streaming/${appName}"
     log.info(s"Initializing Job monitor ZK Path, zkConnect: ${zkConnect} ; " +
       s"zkSessionTimeoutMs: ${zkSessionTimeoutMs} ; zkConnectionTimeoutMs: ${zkConnectionTimeoutMs} ; zkAppPath: ${zkAppPath}")
 
-    /* using receiver mode */
-    //    val topicMap = topics.split(",").map((_, numThreadsPerReceiver.toInt)).toMap
-    //    val kafkaStreams = (1 to numReceivers.toInt).map {
-    //      i => KafkaUtils.createStream(ssc, zkQuorum, group, topicMap, StorageLevel.MEMORY_AND_DISK_SER)
-    //    }
-    //    val unifiedStream = ssc.union(kafkaStreams)
-    //    val lines = unifiedStream.map(_._2)
-
     /* using direct mode */
     val topicsSet = topics.split(",").toSet
     val brokers = "kf4ss.prod.mediav.com:9092,kf5ss.prod.mediav.com:9092,kf6ss.prod.mediav.com:9092"
-    val kafkaParams = Map[String, String](
-      "metadata.broker.list" -> brokers,
-      "serializer.class" -> "kafka.serializer.StringEncoder")
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> brokers,
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> appName,
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
     log.info(s"Initializing kafka Receiver, topics: ${topics} ; brokers: ${brokers} ; kafkaParams: ${kafkaParams.mkString(",")} ")
 
-    val kafkaCluster = new KafkaCluster(kafkaParams)
-    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
+    val kafkaStream = KafkaUtils.createDirectStream[String, String](ssc,
+      LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams))
 
-    /* update Zk Offsets */
-    var offsetRanges = Array[OffsetRange]()
-    val rdd = kafkaStream.transform { rdd =>
-      offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
-      for (offsets <- offsetRanges) {
-        val topicAndPartition = TopicAndPartition(offsets.topic, offsets.partition)
-        val o = kafkaCluster.setConsumerOffsets(appName, Map((topicAndPartition, offsets.fromOffset)))
-        if (o.isLeft) {
-          println(s"Error updating the offset to Kafka cluster: ${o.left.get}")
-        }
-      }
-      rdd
-    }
+    saveToHBase(kafkaStream, htablename)
 
-    saveToHBase(rdd, htablename)
-
-    ssc.addStreamingListener(new StreamingJobMonitor(zkClient, zkAppPath))
+    ssc.addStreamingListener(new StreamingJobMonitor(zkUtils, zkAppPath))
     Runtime.getRuntime.addShutdownHook(new Thread() {
       override def run(): Unit = {
-        ZkUtils.deletePath(zkClient, zkAppPath)
+        zkUtils.deletePath(zkAppPath)
       }
     })
     ssc.start()
     ssc.awaitTermination()
   }
 
-  def saveToHBase(rdd: DStream[(String, String)], htablename: String) = {
-    rdd.map(_._2).foreachRDD((rdd: RDD[String], time: Time) => {
+  def saveToHBase(stream: DStream[org.apache.kafka.clients.consumer.ConsumerRecord[String, String]], htablename: String) = {
+    stream.foreachRDD(rdd => {
+
+      val offsets = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+
+      // some time later, after outputs have completed
+      stream.asInstanceOf[CanCommitOffsets].commitAsync(offsets)
 
       /* convert to Put and save to HBase */
       try {
-        val puts = rdd.flatMap(line => {
+        val puts = rdd.map(record => record.value).flatMap(line => {
           val raw = Base64.getDecoder.decode(line)
-          val ue = LogUtils.ThriftBase64decoder(line, classOf[com.mediav.data.log.unitedlog.UnitedEvent])
+          val ue = com.mediav.data.log.LogUtils.ThriftBase64decoder(line, classOf[com.mediav.data.log.unitedlog.UnitedEvent])
           if (ue != null && ue.isSetAdvertisementInfo && (ue.getAdvertisementInfo.isSetImpressionInfo || ue.getAdvertisementInfo.isSetImpressionInfos)) {
             val eventType = ue.getEventType.getType
             val family = if (eventType == 200) "u"
--- spark-demo/src/main/scala/com/mvad/spark/demo/streaming/StreamingJobMonitor.scala
+++ spark-demo/src/main/scala/com/mvad/spark/demo/streaming/StreamingJobMonitor.scala
@@ -1,44 +1,43 @@
 package com.mvad.spark.demo.streaming
 
-import kafka.utils.ZkUtils
-import org.I0Itec.zkclient.ZkClient
-import org.apache.spark.streaming.StreamingContext
+import kafka.utils._
 import org.apache.spark.streaming.scheduler._
 import org.slf4j.LoggerFactory
+
 
 /**
   * Monitoring Spark Streaming Job
   * put job info/progress to zookeeper
   */
-class StreamingJobMonitor(zkClient: ZkClient, zkAppPath: String) extends StreamingListener {
+class StreamingJobMonitor(zkUtils: ZkUtils, zkAppPath: String) extends StreamingListener {
   val log = LoggerFactory.getLogger(this.getClass)
 
   val receivers = s"${zkAppPath}/receiver"
   val jobProgress = s"${zkAppPath}/progress"
 
   log.info(s"Initializing StreamingJobMonitor , zk Path : ${zkAppPath}...")
-  ZkUtils.makeSurePersistentPathExists(zkClient, receivers)
-  ZkUtils.makeSurePersistentPathExists(zkClient, jobProgress)
+  zkUtils.makeSurePersistentPathExists(receivers)
+  zkUtils.makeSurePersistentPathExists(jobProgress)
 
   override def onReceiverStarted(receiverStarted: StreamingListenerReceiverStarted) {
     val receiver = s"${receivers}/${receiverStarted.receiverInfo.name}"
-    ZkUtils.makeSurePersistentPathExists(zkClient, receiver)
+    zkUtils.makeSurePersistentPathExists(receiver)
   }
 
   override def onReceiverError(receiverError: StreamingListenerReceiverError) {
     val receiver = s"${receivers}/${receiverError.receiverInfo.name}"
-    ZkUtils.updatePersistentPath(zkClient, receiver, "ERROR")
+    zkUtils.updatePersistentPath(receiver, "ERROR")
   }
 
   override def onReceiverStopped(receiverStopped: StreamingListenerReceiverStopped) {
     val receiver = s"${receivers}/${receiverStopped.receiverInfo.name}"
-    ZkUtils.deletePath(zkClient, receiver)
+    zkUtils.deletePath(receiver)
   }
 
   override def onBatchCompleted(batchCompleted: StreamingListenerBatchCompleted) {
     val progressInfo = s"{totalDelay:${batchCompleted.batchInfo.totalDelay.getOrElse(0)}," +
       s"schedulingDelay:${batchCompleted.batchInfo.schedulingDelay.getOrElse(0)}," +
       s"processingDelay:${batchCompleted.batchInfo.processingDelay.getOrElse(0)}"
-    ZkUtils.updatePersistentPath(zkClient, jobProgress, progressInfo)
+    zkUtils.updatePersistentPath(jobProgress, progressInfo)
   }
 }
